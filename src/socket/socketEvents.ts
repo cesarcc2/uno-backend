ed8ad4e550e723e315018a9c07ba6b15e206e13c
cesarcc2 <cmagarinho@gmail.com>
2024-06-16 13:29:40 +0100

    using socket rooms

--- src/socket/socketEvents.ts
+++ src/socket/socketEvents.ts
@@ -7,26 +7,26 @@ import { logger } from "..";
 export function SocketEvents(socket: Socket) {
 
   socket.on(SocketEventTypes.PlayerConnected, (gameId: string) => {
-    // socket.join(gameId);
-    socket.broadcast.emit(SocketEventTypes.SendJoinedGameData, getUpdatedGame(gameId));
+    socket.join(gameId);
+    socket.broadcast.to(gameId).emit(SocketEventTypes.SendJoinedGameData, getUpdatedGame(gameId));
     logger.info("Socket - A player connected ");
   });
 
   socket.on(SocketEventTypes.PlayerDisconnected, (gameId: string) => {
-    socket.broadcast.emit(SocketEventTypes.SendJoinedGameData, getUpdatedGame(gameId));
+    socket.broadcast.to(gameId).emit(SocketEventTypes.SendJoinedGameData, getUpdatedGame(gameId));
     logger.info('Socket - A player disconnected ');
   });
 
   socket.on(SocketEventTypes.PlayerLeft, (gameId: string) => {
-    // socket.leave(gameId);
-    socket.broadcast.emit(SocketEventTypes.SendJoinedGameData, getUpdatedGame(gameId));
+    socket.leave(gameId);
+    socket.broadcast.to(gameId).emit(SocketEventTypes.SendJoinedGameData, getUpdatedGame(gameId));
     logger.info('Socket - A player Left ');
   });
 
   socket.on(SocketEventTypes.PlayerReadyToggle, (data: {playerId: string, gameId: string}) => {
     let updatedGame = togglePlayerReady(data.playerId, data.gameId);
-    socket.broadcast.emit(SocketEventTypes.SendJoinedGameData, updatedGame);
-    socket.emit(SocketEventTypes.SendJoinedGameData, updatedGame);
+    socket.broadcast.to(data.gameId).emit(SocketEventTypes.SendJoinedGameData, updatedGame);
+    socket.to(data.gameId).emit(SocketEventTypes.SendJoinedGameData, updatedGame);
     logger.info('Socket - A player toggled ready button ');
   });
 }
