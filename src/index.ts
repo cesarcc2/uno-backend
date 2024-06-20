7a7bd40947d4b1fb5560bdf62ea097e231976055
cesarcc2 <cmagarinho@gmail.com>
2024-06-20 01:55:15 +0100

    wip

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const server = require('http').createServer(app);
 export const io = new Server(server,{cors: {
   origin: "http://localhost:4200",
 }});
-
+console.log(PORT);
 app.use(express.json());
 
 app.use(cors({
